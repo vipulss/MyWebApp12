6a66596ea6773541cd37b0ca97628ade3ae60627
Noaman Khan <noaman.khan@capgemini.com>
2017-03-01 03:29:45 -0600

    Code updated to support device list

--- src/dotnetCloudantWebstarter/Controllers/HomeController.cs
+++ src/dotnetCloudantWebstarter/Controllers/HomeController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using CloudantDotNet.Common;
+using CloundantDotNet.Models;
 
 namespace CloudantDotNet.Controllers
 {
     public class HomeController : Controller
     {
+    	private readonly string GateType = "GW_GatewayDeviceType";
         public IActionResult Index()
         {
             return View();
+        }
+        
+        public IActionResult Application()
+        {
+            ApiClient client = new ApiClient(ConfigurationManager.AppSettings["ApiKey"], ConfigurationManager.AppSettings["ApiToken"]);
+            List<DeviceList> devicelist = new List<DeviceList>();
+            var arrayList = client.GetAllDevices();
+
+            foreach (var result in arrayList["results"])
+            {
+                DeviceList device = new DeviceList();
+
+                //for (int i = 0; i < result.Count; i++)
+                {
+                    //var deviceCredentails = result[i];
+                    device.Device = result["deviceId"];
+                    device.DeviceType = result["typeId"];
+                    if (device.DeviceType.Equals(GateType))
+                    {
+                        device.Class = "Gateway";
+                    }
+                    else
+                    {
+                        device.Class = "Device";
+                    }
+                }
+                devicelist.Add(device);
+            }
+
+            //devicelist.Add(new DeviceList { Class = "1", Connected = true, Device = "deviceId", DeviceType = "Type" });
+            //devicelist.Add(new DeviceList { Class = "12", Connected = false, Device = "deviceId", DeviceType = "Type" });
+
+            return View(devicelist);
         }
     }
 }
